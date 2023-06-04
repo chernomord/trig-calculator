229b413b5632a970b64dfb95f4ff5180a56de54f
Sviatoslav Komarov <sviatoslavkomarov@cloud.upwork.com>
2023-06-04 13:51:31 +0200

    v2: retain the scope between calculations, clear scope

--- build/v2/script.js
+++ build/v2/script.js
@@ -1,12 +1,18 @@
+const parser = math.parser();
+
 document.getElementById('calcForm').addEventListener('submit', function(event) {
     event.preventDefault();
     calculate();
 });
 
+document.getElementById('reset').addEventListener('click', function() {
+    parser.clear();
+});
+
 function calculate() {
     const input = document.getElementById('input').value;
     try {
-        const result = math.evaluate(input);
+        const result = parser.evaluate(input);
         document.getElementById('result').textContent = result;
     } catch (e) {
         document.getElementById('result').textContent = "Invalid expression";
