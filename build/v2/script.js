ae7accb3700691083b4ef7c368e3536c861f9aab
Sviatoslav Komarov <sviatoslavkomarov@cloud.upwork.com>
2023-06-04 14:28:04 +0200

    v2: add history, clickable results

--- build/v2/script.js
+++ build/v2/script.js
@@ -7,14 +7,43 @@ document.getElementById('calcForm').addEventListener('submit', function(event) {
 
 document.getElementById('reset').addEventListener('click', function() {
     parser.clear();
+    document.getElementById('history').textContent = '';
 });
 
 function calculate() {
-    const input = document.getElementById('input').value;
+    var input = document.getElementById('input').value;
     try {
-        const result = parser.evaluate(input);
+        var result = parser.evaluate(input);
+        var history = document.getElementById('history');
+        var inputNode = document.createElement("span");
+        inputNode.textContent = input + ' = ';
+        inputNode.className = 'input';
+        var resultNode = document.createElement("span");
+        resultNode.textContent = result;
+        resultNode.className = 'result';
+        resultNode.addEventListener('click', function() {
+            navigator.clipboard.writeText(resultNode.textContent);
+            resultNode.className += ' flash';
+            setTimeout(function(){
+                resultNode.className = 'result';
+            }, 1000);
+            notify();
+        });
+        history.appendChild(inputNode);
+        history.appendChild(resultNode);
+        history.appendChild(document.createElement("br"));
         document.getElementById('result').textContent = result;
     } catch (e) {
         document.getElementById('result').textContent = "Invalid expression";
     }
+    document.getElementById('history').scrollTop = document.getElementById('history').scrollHeight;
+}
+
+function notify() {
+    var notification = document.getElementById('notification');
+    notification.style.display = 'block';
+    notification.textContent = 'Copied to clipboard!';
+    setTimeout(function(){
+        notification.style.display = 'none';
+    }, 2000);
 }
