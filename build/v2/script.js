7479cdc0a2a8e964e4c19ff7625820bd7f34d4bd
Sviatoslav Komarov <sviatoslavkomarov@cloud.upwork.com>
2023-06-17 14:23:53 +0200

    v2: semantic highlight

--- build/v2/script.js
+++ build/v2/script.js
@@ -1,8 +1,76 @@
 const parser = math.parser();
 
+let currentInput = '';
+let inputTokens = [];
+
+const functionTokens = ['sin', 'cos', 'tan', 'asin', 'acos', 'atan', 'sqrt', 'log', 'exp', 'abs', 'mod', 'sinh', 'cosh', 'tanh'];
+
+function tokenize(mathExpression) {
+    const tokens = [];
+    const re = /([A-Za-z_][A-Za-z0-9_]*)|(\d*\.\d+|\d+)|(\S)|(\s+)/g;
+    let match;
+
+    while (match = re.exec(mathExpression)) {
+        let token = {value: match[0]};
+        if (match[1]) {
+            if (functionTokens.includes(match[1])) {
+                token.type = 'function';
+            } else {
+                token.type = 'variable';
+            }
+        } else if (match[2]) {
+            token.type = 'number';
+        } else if (match[3]) {
+            switch (match[3]) {
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '^':
+                    token.type = 'operator';
+                    break;
+                case '(':
+                case ')':
+                    token.type = 'parentheses';
+                    break;
+                default:
+                    token.type = 'unknown';
+            }
+        } else if (match[4]) {
+            token.type = 'space';
+            token.value = '\u00A0'.repeat(match[4].length);
+        }
+        tokens.push(token);
+    }
+
+    return tokens;
+}
+
+function renderTokenized(tokens) {
+    const output = document.getElementById('highlighted');
+    output.innerHTML = '';
+    tokens.forEach(token => {
+        const span = document.createElement('span');
+        span.textContent = token.value;
+        span.classList.add('token');
+        span.classList.add(token.type);
+        output.appendChild(span);
+    });
+}
+
 document.getElementById('calcForm').addEventListener('submit', function(event) {
     event.preventDefault();
     calculate();
+});
+
+// select your input field and div
+const inputField = document.querySelector('#input');
+const div = document.querySelector('#highlighted');
+
+// add an event listener to your input field
+inputField.addEventListener('scroll', function() {
+    // synchronize the scroll position of the div with the input field
+    div.scrollLeft = inputField.scrollLeft;
 });
 
 const input = document.getElementById('input');
@@ -17,12 +85,19 @@ input.addEventListener('keydown', function(event) {
             '{': '}',
         }[event.key];
         input.setRangeText(wrappedText, start, end, 'select');
+        updateInput(input.value);
         event.preventDefault();
     }
 });
 
+function updateInput(inputValue = '') {
+    inputTokens = tokenize(inputValue);
+    renderTokenized(inputTokens);
+}
+
 input.addEventListener('input', function(e) {
     e.target.value = e.target.value.toLowerCase();
+    updateInput(e.target.value);
 });
 
 function calculate() {
@@ -48,11 +123,13 @@ function calculate() {
         history.appendChild(resultNode);
         history.appendChild(document.createElement("br"));
         document.getElementById('result').textContent = result;
+
+        document.getElementById('history').scrollTop = document.getElementById('history').scrollHeight;
+        document.getElementById('input').value = '';
+        updateInput()
     } catch (e) {
         document.getElementById('result').textContent = "Invalid expression";
     }
-    document.getElementById('history').scrollTop = document.getElementById('history').scrollHeight;
-    document.getElementById('input').value = '';
 }
 
 function notify() {
@@ -67,6 +144,8 @@ function notify() {
 const buttonsCommandsEnum ={
     clear() {
         parser.clear();
+        document.getElementById('input').value = '';
+        updateInput();
         document.getElementById('history').textContent = '';
     },
     sin()   { insertFunction(input, 'sin') },
@@ -97,4 +176,5 @@ function insertFunction(inputEl, funcName) {
     const selectedText = inputEl.value.slice(start, end);
     const wrappedText = funcName + '(' + selectedText + ')';
     inputEl.setRangeText(wrappedText, start, end, 'select');
+    updateInput(inputEl.value);
 }
